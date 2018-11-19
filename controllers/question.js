5555221a1b09079291356f3322ef675cd4d90eb1
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-11-19 16:09:42 -0500

    Repaso - Creación del modelo y controlador para preguntas

---
+++ controllers/question.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const questions = require('../model/index').questions
+
+async function createQuestion (req, h) {
+  let result
+
+  try {
+    result = await questions.create(req.payload, req.state.user)
+    console.log(`Pregunta creada con el ID: ${result}`)
+  } catch (error) {
+    console.error(`Ocurrio un error : ${error}`)
+
+    return h.view('ask', {
+      title: 'Crear Pregunta',
+      error: 'Problema creando la pregunta'
+    }).code(500).takeover()
+  }
+
+  return h.response(`Pregunta creada ID: ${result}`)
+}
+
+module.exports = {
+  createQuestion: createQuestion
+}
---
+++ model/questions.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class Questions {
+  constructor (db) {
+    this.db = db
+    this.ref = this.db.ref('/')
+    this.collection = this.ref.child('questions')
+  } 
+  
+  async create (data, user) {
+    data.owner = user
+    const question = this.collection.push() // Crear nueva referencia
+    question.set(data)
+
+    return question.key
+  }
+} 
+module.exports = Questions
